0f3a1c358406d64e685402241fb6dc97af38c066
Theo Schlossnagle <jesus@omniti.com>
2011-03-15 23:08:26 -0400

    Do this the most evil way possible.  This appear to work in my tests. libgmp fellas are being unfortunately uncooperative in improving their software, so it make sense to have a tenable solution without them.
    This can and will badly malfunction at some point, I'm not sure I ever
    want to run this in production -- time will tell.
    
    This uses signals catching the SIGABRT and SIGFPE (the two raised in
    libgmp) and siglongjmps out of them.  In single threaded programs the
    SIGFPE should be entirely safe, the SIGABRT is a bit more risky.  We
    limit the siglongjmp to when we know we've called into libgmp.
    
    And I will end this with a parting note: libgmp can suck it.

--- node_gmp.cc
+++ node_gmp.cc
@@ -6,19 +6,86 @@
 #include <sys/mman.h>
 #include <iostream>
 #include <alloca.h>
+#include <errno.h>
+#include <setjmp.h>
 
 #include "node_gmp.h"
 
-void node_gmp_replace_abort_cxx() {
-  throw "gmp abort";
-}
+static int inside_gmp_call = 0;
+#define rewire(symbol, func, block) do { \
+  void **sym = (void **)dlsym(RTLD_DEFAULT, #symbol); \
+  if(!sym) block \
+  else (*sym) = (void *)func; \
+} while(0)
+
+#define evil_try_catch(code) do { \
+  if(sigsetjmp(gmp_sucks_env,0) == 0) { \
+    inside_gmp_call = 1; \
+    code \
+    inside_gmp_call = 0; \
+  } else { \
+    inside_gmp_call = 0; \
+    return ThrowException(Exception::Error(String::New("gmp abort"))); \
+  } \
+} while(0)
+
 extern "C" {
+  static sigjmp_buf gmp_sucks_env;
+  static struct sigaction old_actions[32] = { { 0 } };
+  extern void __gmp_invalid_operation();
+
+  /* rewrite the default allocators */
   extern void *__gmp_default_allocate(size_t);
   extern void *__gmp_default_reallocate(void *, size_t, size_t);
-  extern void __gmp_invalid_operation();
+  extern void  __gmp_default_free(void *, size_t);
+  void node_gmp_replace_abort_cxx() {
+    siglongjmp(gmp_sucks_env, 1);
+  }
+  void *node_gmp_replace_allocate(size_t s) {
+    void *answer = __gmp_default_allocate(s);
+    if(answer == NULL) node_gmp_replace_abort_cxx();
+    return answer;
+  }
+  void *node_gmp_replace_reallocate(void *p, size_t o, size_t n) {
+    void *answer = __gmp_default_reallocate(p,o,n);
+    if(answer == NULL) node_gmp_replace_abort_cxx();
+    return answer;
+  }
+  void node_gmp_replace_free(void *p, size_t s) {
+    __gmp_default_free(p, s);
+  }
   extern void replace_abort() {
     node_gmp_replace_abort_cxx();
   }
+  void node_gmp_sigthrow(int sig, siginfo_t *info, void *c) {
+    if(inside_gmp_call) {
+      siglongjmp(gmp_sucks_env, 1);
+    }
+    if(old_actions[sig].sa_handler != NULL &&
+       old_actions[sig].sa_handler != SIG_IGN) {
+      if(old_actions[sig].sa_handler == SIG_DFL) {
+        struct sigaction new_action = { 0 };
+        new_action.sa_handler = SIG_DFL;
+        sigaction(sig, &new_action, NULL);
+        raise(sig);
+      }
+      else {
+        old_actions[sig].sa_sigaction(sig, info, c);
+      }
+    }
+  }
+  void rewire_signal(int sig) {
+    struct sigaction new_action;
+    if(sigaction(sig, NULL, &old_actions[sig]) == 0) {
+      new_action = old_actions[sig];
+      new_action.sa_sigaction = node_gmp_sigthrow;
+      sigaction(sig, &new_action, NULL);
+    }
+    else {
+      fprintf(stderr, "sigaction failed (gmp is evil): %s\n", strerror(errno));
+    }
+  }
+
 #define abort_replace(f) abort_replace_func(#f, f)
   int abort_replace_func(const char *name, void *f) {
     void ** code = (void **)f;
@@ -38,8 +105,6 @@ extern "C" {
       icrutch = (unsigned long long int) code;
       offset = (icrutch % psize);
       if(icrutch/psize > startpoint/psize) {
-        fprintf(stderr, "abort_replace_func: no abort within first page %s\n",
-                name);
         return -1;
       }
       if(*code == (void *)abrt_code) break;
@@ -47,6 +112,7 @@ extern "C" {
     }
     if(mprotect((void *)(icrutch - offset), psize,
                 PROT_READ|PROT_WRITE|PROT_EXEC) == 0) {
+      fprintf(stderr, "rewiring abort @ %p\n", code);
       *code = (void *)replace_abort;
       mprotect((void *)(icrutch - offset), psize, PROT_READ|PROT_EXEC);
       return 0;
@@ -105,11 +171,7 @@ GInt::Add(const Arguments &args) {
   GETARG(args[0], strtok(*val, "."));
 
   GInt *self = ObjectWrap::Unwrap<GInt>(args.This());
-  try {
-    self->val_ += i;
-  } catch (char *err) {
-    return ThrowException(Exception::Error(String::New(err)));
-  }
+  evil_try_catch({ self->val_ += i; });
   return args.This();
 }
 
@@ -122,11 +184,7 @@ GInt::Sub(const Arguments &args) {
   GETARG(args[0], strtok(*val, "."));
 
   GInt *self = ObjectWrap::Unwrap<GInt>(args.This());
-  try {
-    self->val_ -= i;
-  } catch (char *err) {
-    return ThrowException(Exception::Error(String::New(err)));
-  }
+  evil_try_catch({ self->val_ -= i; });
 
   return args.This();
 }
@@ -140,11 +198,7 @@ GInt::Mul(const Arguments &args) {
   GETARG(args[0], strtok(*val, "."));
 
   GInt *self = ObjectWrap::Unwrap<GInt>(args.This());
-  try {
-    self->val_ *= i;
-  } catch (char *err) {
-    return ThrowException(Exception::Error(String::New(err)));
-  }
+  evil_try_catch({ self->val_ *= i; });
 
   return args.This();
 }
@@ -158,11 +212,7 @@ GInt::Div(const Arguments &args) {
   GETARG(args[0], strtok(*val, "."));
 
   GInt *self = ObjectWrap::Unwrap<GInt>(args.This());
-  try {
-    self->val_ /= i;
-  } catch (char *err) {
-    return ThrowException(Exception::Error(String::New(err)));
-  }
+  evil_try_catch({ self->val_ /= i; });
 
   return args.This();
 }
@@ -177,7 +227,8 @@ GInt::Pow(const Arguments &args) {
 
   GInt *self = ObjectWrap::Unwrap<GInt>(args.This());
 
-  try {
+  evil_try_catch({
+    inside_gmp_call = 1;
     mpz_t c;
     mpz_init(c);
 
@@ -186,9 +237,7 @@ GInt::Pow(const Arguments &args) {
     self->val_ = mpz_class(c);
 
     mpz_clear(c);
-  } catch (char *err) {
-    return ThrowException(Exception::Error(String::New(err)));
-  }
+  });
 
   return args.This();
 }
@@ -200,18 +249,13 @@ GInt::ToString(const Arguments &args) {
   GInt *self = ObjectWrap::Unwrap<GInt>(args.This());
   Local<String> str;
 
-  try {
-    str = String::New(self->val_.get_str(10).c_str());
-  } catch (char *err) {
-    return ThrowException(Exception::Error(String::New(err)));
-  }
+  evil_try_catch({ str = String::New(self->val_.get_str(10).c_str()); });
   return scope.Close(str);
 }
 
 Handle<Value>
 GFloat::New(const Arguments &args) {
   HandleScope scope;
-
   mpf_class i = 0;
 
   GETARG(args[0], *val);
@@ -241,11 +285,7 @@ GFloat::Add(const Arguments &args) {
 
   GFloat *self = ObjectWrap::Unwrap<GFloat>(args.This());
 
-  try {
-    self->val_ += i;
-  } catch (char *err) {
-    return ThrowException(Exception::Error(String::New(err)));
-  }
+  evil_try_catch({ self->val_ += i; });
 
   return args.This();
 }
@@ -260,11 +300,7 @@ GFloat::Sub(const Arguments &args) {
 
   GFloat *self = ObjectWrap::Unwrap<GFloat>(args.This());
 
-  try {
-    self->val_ -= i;
-  } catch (char *err) {
-    return ThrowException(Exception::Error(String::New(err)));
-  }
+  evil_try_catch({ self->val_ -= i; });
 
   return args.This();
 }
@@ -279,11 +315,7 @@ GFloat::Mul(const Arguments &args) {
 
   GFloat *self = ObjectWrap::Unwrap<GFloat>(args.This());
 
-  try {
-    self->val_ *= i;
-  } catch (char *err) {
-    return ThrowException(Exception::Error(String::New(err)));
-  }
+  evil_try_catch({ self->val_ *= i; });
 
   return args.This();
 }
@@ -298,11 +330,7 @@ GFloat::Div(const Arguments &args) {
 
   GFloat *self = ObjectWrap::Unwrap<GFloat>(args.This());
 
-  try {
-    self->val_ /= i;
-  } catch (char *err) {
-    return ThrowException(Exception::Error(String::New(err)));
-  }
+  evil_try_catch({ self->val_ /= i; });
 
   return args.This();
 }
@@ -317,7 +345,7 @@ GFloat::Pow(const Arguments &args) {
 
   GFloat *self = ObjectWrap::Unwrap<GFloat>(args.This());
 
-  try {
+  evil_try_catch({
     mpf_t c;
     mpf_init(c);
 
@@ -326,9 +354,7 @@ GFloat::Pow(const Arguments &args) {
     self->val_ = mpf_class(c);
 
     mpf_clear(c);
-  } catch (char *err) {
-    return ThrowException(Exception::Error(String::New(err)));
-  }
+  });
 
   return args.This();
 }
@@ -350,11 +376,7 @@ GFloat::ToString(const Arguments &args) {
   long int i;
   GFloat *self = ObjectWrap::Unwrap<GFloat>(args.This());
   const char *unfixed = NULL;
-  try {
-    unfixed = self->val_.get_str(exp,10).c_str();
-  } catch (char *err) {
-    return ThrowException(Exception::Error(String::New(err)));
-  }
+  evil_try_catch({ unfixed = self->val_.get_str(exp,10).c_str(); });
   char *buff = (char *)alloca(abs(exp) + 2 + strlen(unfixed));
   char *cp = buff;
   if(unfixed[0] == '-') { *cp++ = '-'; unfixed++; }
@@ -408,19 +430,23 @@ void RegisterModule(Handle<Object> target) {
 
   target->Set(String::NewSymbol("Float"), t_float->GetFunction());
 
+  /* rewire allocators to throw on OOM */
+  rewire(__gmp_allocate_func, node_gmp_replace_allocate,
+         { ThrowException(Exception::Error(String::New("cannot find __gmp_allocate_func"))); });
+  rewire(__gmp_reallocate_func, node_gmp_replace_reallocate,
+         { ThrowException(Exception::Error(String::New("cannot find __gmp_reallocate_func"))); });
+  rewire(__gmp_free_func, node_gmp_replace_free, { });
+
+  /* The following is a best effort at fixing the FPE condition */
 #ifdef gmp_initialize_abort
   gmp_initialize_abort(replace_abort);
 #else
+  rewire_signal(SIGABRT);
+  rewire_signal(SIGFPE);
 #warning sketchy abort replacements going on
-  if(((sizeof (unsigned long) > sizeof (int)) &&
-      abort_replace((void *)mpz_init2)) ||
-     abort_replace((void *)mpz_realloc) ||
-     abort_replace((void *)mpz_realloc2) ||
-     abort_replace((void *)__gmp_default_allocate) ||
-     abort_replace((void *)__gmp_default_reallocate) ||
-     abort_replace((void *)__gmp_invalid_operation)) {
-    ThrowException(Exception::Error(String::New("node-gmp could not subvert abort")));
-  }
+  if(sizeof (unsigned long) > sizeof (int))
+    abort_replace((void *)mpz_init2);
+  abort_replace((void *)__gmp_invalid_operation);
 #endif
 }
 
